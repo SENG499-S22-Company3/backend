b36a94ab712a3e1184b8a6ac2a6c3f91a081472e
Aomi Jokoji <aomi@uvic.ca>
2022-06-01 11:12:59 -0700

    doc: hints for implementation

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,12 +1,14 @@
 import { Request } from "express";
 
 export interface Context {
-  // example
+  // example context value
   username?: string;
   // TODO: add context (aka. prisma, auth, etc.)
 }
 
 export async function createContext(req: Request): Promise<Context> {
+  // TODO: add any request specific context (aka. prisma, auth, etc.)
+  // TODO: check for session with cookie
   return {
     username: "some username",
   };
