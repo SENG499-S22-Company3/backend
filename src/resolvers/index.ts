70fcb8ba62a8842eba85a2240ea949557a668712
James Lacroix <lacroix.james@icloud.com>
2022-06-20 15:11:37 -0700

    Revert " Query Resolvers (#53)"
    This reverts commit 18560007875feca34c49fb22d6fc2ad1b9b8272b.
    
    There were some linting and logic problems with it. Not ready for merge.

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,6 +1,5 @@
 import { Context } from '../context';
-import { CourseId, Resolvers, Role } from '../schema';
-import { prisma, lookupUser } from '../prisma';
+import { Resolvers } from '../schema';
 import { login, createNewUser, changePassword } from '../auth';
 import * as utils from '../utils';
 
@@ -23,75 +22,10 @@ const noPerms = {
 
 export const resolvers: Resolvers<Context> = {
   Query: {
-    me: async (_, _params, ctx) => {
-      const user = await prisma.user.findUnique({
-        where: { username: ctx.session.user?.username }, //replace testuser with session.username?
-      });
-      if (!user) return null;
-
-      return {
-        id: user.id,
-        username: user.username,
-        password: user.password,
-        role: user.role as Role,
-        preferences: [],
-        active: user.active
-      };
-    },
-    findUserById: async (_, _params, ctx) => {
-
-      const finduser = await prisma.user.findUnique({
-        where: { id: +_params.id},
-      });
-
-      if (!finduser) return null;
-
-      return {
-        id: finduser.id,
-        username: finduser.username,
-        password: finduser.password,
-        role: finduser.role as Role,
-        preferences: [],
-        active: finduser.active
-      };
-
-    },
-    courses: async (_, _params, ctx) => {
-     
-      const courses = await prisma.course.findMany({
-          where: { term: _params.term || undefined },
-          include: { courseSection: true, coursePreference: true}
-      });
-
-      
-      return [{
-        CourseID: courses[0].courseSection[0].courseId as unknown as CourseId,
-        hoursPerWeek: courses[0].courseSection[0].hoursPerWeek,
-        capacity: courses[0].courseSection[0].capacity,
-        professors: [], 
-        startDate: courses[0].courseSection[0].startDate,
-        endDate: courses[0].courseSection[0].endDate,
-        meetingTimes: []
-      }]
-    
-      
-    },
-    schedule: async (_, _params, ctx) => {
-     
-      const schedule = await prisma.schedule.findUnique({
-          where: {},
-          include: {courseSection: true}
-      });
-
-      if (!schedule) return null;
-
-      return{
-        id: "test", // schedule.id.toString not working
-        year: schedule.year,
-        createdAt: schedule.createdOn,
-        courses: []
-
-      }
+    me: (_, _params, ctx) => {
+      if (!ctx.session.user) return null;
+      console.log(ctx.session.user.username);
+      return null;
     },
   },
   Mutation: {
