035f6be8d7ecf2b5e4f95b1289b66b95b9071fb7
MustafaWasif <mustafa.wasif123@gmail.com>
2022-06-24 12:37:07 -0700

    Api Client

---
+++ src/generated-sources/openapi/api/api.ts
@@ -0,0 +1,19 @@
+export * from './defaultApi';
+import { DefaultApi } from './defaultApi';
+import * as http from 'http';
+import { Schedule } from '../api';
+
+export class HttpError extends Error {
+  constructor(
+    public response: http.IncomingMessage,
+    public body: any,
+    public statusCode?: number
+  ) {
+    super('HTTP request failed');
+    this.name = 'HttpError';
+  }
+}
+
+export { RequestFile } from '../model/models';
+
+export const APIS = [DefaultApi];
