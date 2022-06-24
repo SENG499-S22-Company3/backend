51061b878a389865a93fd85c6e7418e7a0df3421
kendajani <47232534+kendajani@users.noreply.github.com>
2022-06-24 15:20:44 -0700

    Seed database with historical data (#57)
    * Added historical data json
    
    * Added date-fns
    
    * Turned resolveJsonModule option on in ts config
    
    * Updated schema prisma
    
    * Created migration files
    
    * Created seeding util functions
    
    * Added course section seeding
    
    * Fixed linting errors
    
    * made display name into an optional field
    
    * Added a single schedule to seed
    
    * Connected seeded sections to schedule and updates

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,95 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Term, Role } from '@prisma/client';
 import bcrypt from 'bcrypt';
-import { Role } from '../src/schema';
+import historicalData from '../data/historical-data-2019.json';
+import { addCourseSections } from '../src/utils';
 
 const prisma = new PrismaClient();
 
 async function seedUsers(): Promise<void> {
-  // create default development admin user
-  await prisma.user.create({
-    data: {
+  // create default users
+
+  // Admin
+  await prisma.user.upsert({
+    where: {
+      username: 'testadmin',
+    },
+
+    create: {
+      displayName: 'Test Admin',
       username: 'testadmin',
       password: await bcrypt.hash('testpassword', 10),
       active: true,
       hasPeng: false,
-      role: Role.Admin,
+      role: Role.ADMIN,
+    },
+
+    update: {
+      displayName: 'Test Admin',
+      username: 'testadmin',
+      password: await bcrypt.hash('testpassword', 10),
+      active: true,
+      hasPeng: false,
+      role: Role.ADMIN,
     },
   });
 
-  await prisma.user.create({
-    data: {
+  // User
+  await prisma.user.upsert({
+    where: {
+      username: 'testuser',
+    },
+
+    create: {
+      displayName: 'Test user',
       username: 'testuser',
       password: await bcrypt.hash('testpassword', 10),
       active: true,
       hasPeng: false,
-      role: Role.User,
+      role: Role.USER,
+    },
+
+    update: {
+      displayName: 'Test user',
+      username: 'testuser',
+      password: await bcrypt.hash('testpassword', 10),
+      active: true,
+      hasPeng: false,
+      role: Role.USER,
     },
   });
 }
 
+async function seedSections(): Promise<void> {
+  const fallSections = historicalData?.fallTermCourses;
+  const springSections = historicalData?.springTermCourses;
+  const summerSections = historicalData?.summerTermCourses;
+
+  const fall: Term = 'FALL';
+  const spring: Term = 'SPRING';
+  const summer: Term = 'SUMMER';
+
+  // Create a schedule object
+  await prisma.schedule.upsert({
+    where: {
+      id: 1,
+    },
+    create: {
+      year: 2019,
+    },
+    update: {},
+  });
+
+  // Add a years worth of course sections
+  await Promise.all([
+    addCourseSections(fallSections, fall),
+    addCourseSections(springSections, spring),
+    addCourseSections(summerSections, summer),
+  ]);
+}
+
 // A `main` function so that you can use async/await
 async function main() {
-  await seedUsers();
+  await Promise.all([seedUsers(), seedSections()]);
 }
 
 main()
