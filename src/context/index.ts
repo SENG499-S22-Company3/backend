a53844cf8c08101583b494c3bbfb4ff61e9beb16
Aomi Jokoji <aomi@uvic.ca>
2022-06-25 18:08:20 -0700

    feat: algo1 and 2 client

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,11 +1,20 @@
 import { Request } from 'express';
 import { Session, SessionData } from 'express-session';
+import { DefaultApi as Algo1 } from '../client/algorithm1/api';
+import { DefaultApi as Algo2 } from '../client/algorithm2/api';
+
+// Instantiate api clients
+const algorithm1 = new Algo1(process.env.ALGO1_API_URL);
+const algorithm2 = new Algo2(process.env.ALGO1_API_URL);
 
 export interface Context {
   // TODO: keep here until we have login implemented. a partial given it could be undefined
   session: Session & Partial<SessionData>;
   // TODO: add context (aka. prisma, auth, etc.)
   logout: () => Promise<void>;
+  // dependencies
+  algorithm1: Algo1;
+  algorithm2: Algo2;
 }
 
 export async function createContext({
@@ -19,5 +28,7 @@ export async function createContext({
   return {
     session,
     logout: async () => new Promise((resolve) => session.destroy(resolve)),
+    algorithm1,
+    algorithm2,
   };
 }
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,6 @@
 import { Context } from '../context';
 import { Resolvers } from '../schema';
-import { Algorithm1Api } from '../clientAPI/openapi/algorithm1/api';
-// import { Algorithm2API } from '../clientAPI/openapi/algorithm2/api';
+
 import {
   login,
   createNewUser,
@@ -10,10 +9,6 @@ import {
 } from '../auth';
 import * as utils from '../utils';
 import { getSchedule, getCourses, getMe, getUserByID } from './resolverUtils';
-
-// Instanciate api clients
-const apiClientAlg1: Algorithm1Api = new Algorithm1Api();
-// const apiClientAlg2: Algorithm2API = new Algorithm2API();
 
 const noLogin = {
   success: false,
@@ -85,10 +80,9 @@ export const resolvers: Resolvers<Context> = {
       if (!ctx.session.user) return noLogin;
       else if (!utils.isAdmin(ctx.session.user)) return noPerms; // Only Admin can generate schedule
 
-      // Call Algorithm 1 API
-      // schedulePost requires 3 parameters - {Historic data}, {coursesToSchedule} & [professors]
-      const response = apiClientAlg1.schedulePost();
-      console.log('Response from Alg1', response);
+      const { response, body } = await ctx.algorithm1.schedulePost();
+      console.log('response(alg1)', JSON.stringify(response));
+      console.log('body(alg1)', JSON.stringify(body));
       if (!response) return EncounteredError;
 
       return generateSchedule(_params.input);
