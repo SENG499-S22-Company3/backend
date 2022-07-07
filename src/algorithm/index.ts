80b830961b41c0c31a86b7a2fd0e568c83e50509
Aomi <aomi@users.noreply.github.com>
2022-07-06 18:41:02 -0700

    feat: graphql codegen package updates and algorithm client setup (#91)
    * chore: update graphql-codegen pacckages
    
    * chore: run graphql codegen and format
    
    * chore: update openapi scripts path
    
    * chore: update api sources
    
    * chore: regenerate client
    
    * feat: regenerate client
    
    * feat: update context for algo
    
    * fix: move to dev dependencies
    
    * chore: stop using generated clients

--- src/algorithm/index.ts
+++ src/algorithm/index.ts
@@ -1,12 +1,31 @@
-import { DefaultApi as Algo1 } from '../client/algorithm1';
-import { DefaultApi as Algo2 } from '../client/algorithm2';
+import { DefaultApi as Algo1 } from '../client/algorithm1/api';
+import { DefaultApi as Algo2 } from '../client/algorithm2/api';
+
+const algoUrl = {
+  algorithm1: {
+    company3: process.env.ALGO1_COMPANY_3_URL || 'http://localhost:8080',
+    company4: process.env.ALGO1_COMPANY_4_URL || 'http://localhost:8080',
+  },
+  algorithm2: {
+    company3: process.env.ALGO2_COMPANY_3_URL || 'http://localhost:8080',
+    company4: process.env.ALGO2_COMPANY_4_URL || 'http://localhost:8080',
+  },
+};
+console.info('Setting up Algorithm APIs...');
+console.info('Algorithm 1 (Company 3) ->', algoUrl.algorithm1.company3);
+console.info('Algorithm 1 (Company 4) ->', algoUrl.algorithm1.company4);
+console.info('Algorithm 2 (Company 3) ->', algoUrl.algorithm2.company3);
+console.info('Algorithm 2 (Company 4) ->', algoUrl.algorithm2.company4);
 
 // Instantiate api clients
-export const algorithm1 = new Algo1(
-  undefined,
-  process.env.ALGO1_URL || 'http://localhost:8080'
-);
-export const algorithm2 = new Algo2(
-  undefined,
-  process.env.ALGO2_URL || 'http://localhost:5000'
-);
+
+export const algorithm = {
+  algo1: {
+    company3: new Algo1(undefined, algoUrl.algorithm1.company3),
+    company4: new Algo1(undefined, algoUrl.algorithm1.company4),
+  },
+  algo2: {
+    company3: new Algo2(undefined, algoUrl.algorithm2.company3),
+    company4: new Algo2(undefined, algoUrl.algorithm2.company4),
+  },
+};
