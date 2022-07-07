1c544101a8f0cc0725f40271be356a98b2e0f2f8
Aomi Jokoji <aomi@uvic.ca>
2022-07-06 17:47:45 -0700

    feat: update context for algo

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -99,10 +99,76 @@ export const resolvers: Resolvers<Context> = {
       if (!ctx.session.user) return noLogin;
       else if (!utils.isAdmin(ctx.session.user)) return noPerms; // Only Admin can generate schedule
 
+      const res = await ctx.algorithm.algo1.company3.schedulePost({
+        coursesToSchedule: {
+          fallCourses: [],
+          springCourses: [],
+          summerCourses: [
+            {
+              courseNumber: '440',
+              subject: 'SENG',
+              sequenceNumber: 'A01',
+              courseTitle: 'Embedded Systems',
+              courseCapacity: 0,
+              numSections: 1,
+            },
+            {
+              courseNumber: '499',
+              subject: 'SENG',
+              sequenceNumber: 'A01',
+              courseTitle: 'Design Project II',
+              courseCapacity: 0,
+              numSections: 1,
+            },
+          ],
+        },
+        hardScheduled: {
+          fallCourses: [],
+          springCourses: [],
+          summerCourses: [],
+        },
+        // professors: [
+        //   {
+        //     displayName: 'John Doe',
+        //     preferences: [
+        //       {
+        //         courseNum: '499',
+        //         term: 'FALL',
+        //         preferenceNum: 1,
+        //       },
+        //     ],
+        //     fallTermCourses: 1,
+        //   },
+        // ],
+        professors: (
+          await prisma.teachingPreference.findMany({
+            include: {
+              coursePreference: {
+                include: {
+                  course: true,
+                },
+              },
+              User: true,
+            },
+          })
+        )
+          .map((pref) => ({
+            displayName: pref.User?.displayName,
+            preferences: pref.coursePreference?.map((p) => ({
+              courseNum: p.course.subject + p.course.courseCode,
+              term: p.course.term,
+              preferenceNum: p.preference,
+            })),
+          }))
+          .filter((p) => p.preferences.length > 0 || !p.displayName) as any,
+      });
+      console.log(res.statusText);
+      console.log(res.data);
+
       try {
         const baseUrl = 'https://schedulater-algorithm1.herokuapp.com';
         const response = await axios.post<Schedule>(
-          `${baseUrl}/generate`,
+          `${baseUrl}/schedule`,
           minInput
         );
         const schedule = await prisma.schedule.create({
