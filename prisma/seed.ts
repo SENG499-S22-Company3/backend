dd89457aba387f84b6cce6c59d394933fc7fb55c
Aomi <aomi@users.noreply.github.com>
2022-07-21 13:12:06 -0700

    feat: Update seeding script with base courses and users (profs) (#128)
    * feat: add base courses
    
    * feat: simplify seeding script
    assumes reset of db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,132 +1,90 @@
-import { PrismaClient, Term, Role } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
 import bcrypt from 'bcrypt';
-import historicalData from '../data/historical-data-2019.json';
 import teachingPref from '../data/teacher-pref-data.json';
-import {
-  addCourseSections,
-  addTeachingAndCoursePreferences,
-} from '../src/utils';
+import baseCourses from '../data/base_courses.json';
+import { getSeqNumber } from '../src/utils';
 
 const prisma = new PrismaClient();
+const hash = bcrypt.hashSync('testpassword', 10);
 
 async function seedUsers(): Promise<void> {
   // create default users
-
   // Admin
-  await prisma.user.upsert({
-    where: {
-      username: 'testadmin',
-    },
+  const admin = {
+    displayName: 'Test Admin',
+    username: 'testadmin',
+    password: hash,
+    active: true,
+    hasPeng: false,
+    role: Role.ADMIN,
+  };
 
-    create: {
-      displayName: 'Test Admin',
-      username: 'testadmin',
-      password: await bcrypt.hash('testpassword', 10),
-      active: true,
-      hasPeng: false,
-      role: Role.ADMIN,
-    },
+  const user = {
+    displayName: 'Test user',
+    username: 'testuser',
+    password: hash,
+    active: true,
+    hasPeng: false,
+    role: Role.USER,
+  };
 
-    update: {
-      displayName: 'Test Admin',
-      username: 'testadmin',
-      password: await bcrypt.hash('testpassword', 10),
-      active: true,
-      hasPeng: false,
-      role: Role.ADMIN,
-    },
+  const tbd = {
+    displayName: 'TBD',
+    username: 'TBD',
+    password: hash,
+    active: false,
+    hasPeng: false,
+    role: Role.USER,
+  };
+
+  const res = await prisma.user.createMany({
+    data: [admin, user, tbd],
   });
-
-  // User
-  await prisma.user.upsert({
-    where: {
-      username: 'testuser',
-    },
-
-    create: {
-      displayName: 'Test user',
-      username: 'testuser',
-      password: await bcrypt.hash('testpassword', 10),
-      active: true,
-      hasPeng: false,
-      role: Role.USER,
-    },
-
-    update: {
-      displayName: 'Test user',
-      username: 'testuser',
-      password: await bcrypt.hash('testpassword', 10),
-      active: true,
-      hasPeng: false,
-      role: Role.USER,
-    },
-  });
-
-  // Special tbd User for courses without profs
-  await prisma.user.upsert({
-    where: {
-      username: 'TBD',
-    },
-
-    create: {
-      displayName: 'TBD',
-      username: 'TBD',
-      password: await bcrypt.hash('testpassword', 10),
-      active: false,
-      hasPeng: false,
-      role: Role.USER,
-    },
-
-    update: {
-      displayName: 'TBD',
-      username: 'TBD',
-      password: await bcrypt.hash('testpassword', 10),
-      active: false,
-      hasPeng: false,
-      role: Role.USER,
-    },
-  });
+  console.log(`Seeded ${res.count} users`);
 }
 
-async function seedPref() {
-  for (const professor of teachingPref.professors) {
-    await addTeachingAndCoursePreferences(professor);
-  }
+async function seedCourses(): Promise<void> {
+  const res = await prisma.course.createMany({
+    data: baseCourses.map((course) => ({
+      subject: course.subject,
+      courseCode: course.code,
+      title: course.title,
+      streamSequence: getSeqNumber(course.subject, course.code),
+      term: course.term as any,
+    })),
+    skipDuplicates: true,
+  });
+  console.log(`Seeded ${res.count} courses`);
 }
 
-async function seedSections(): Promise<void> {
-  const fallSections = historicalData?.fallTermCourses;
-  const springSections = historicalData?.springTermCourses;
-  const summerSections = historicalData?.summerTermCourses;
-
-  const fall: Term = 'FALL';
-  const spring: Term = 'SPRING';
-  const summer: Term = 'SUMMER';
-
-  // Create a schedule object
-  await prisma.schedule.upsert({
-    where: {
-      id: 1,
-    },
-    create: {
-      year: 2019,
-    },
-    update: {},
+async function seedProfessors(): Promise<void> {
+  const { professors } = teachingPref;
+  const res = await prisma.user.createMany({
+    data: professors.map((professor) => ({
+      displayName: professor.displayName,
+      username: professor.displayName
+        // replace with generated username
+        .replace(/[\s|,]/g, '')
+        .toLocaleLowerCase(),
+      password: hash,
+      active: true,
+      hasPeng: false,
+      role: Role.USER,
+    })),
+    skipDuplicates: true,
   });
-
-  // Add a years worth of course sections
-  await Promise.all([
-    addCourseSections(fallSections, fall),
-    addCourseSections(springSections, spring),
-    addCourseSections(summerSections, summer),
-  ]);
+  console.log(`Seeded ${res.count} professors`);
 }
 
 // A `main` function so that you can use async/await
 async function main() {
-  await Promise.all([seedUsers(), seedSections()]);
-
-  await seedPref();
+  // users used for testing and ci
+  console.log('Seeding base users (admin, user, tbd)...');
+  await seedUsers();
+  console.log('Seeding courses...');
+  await seedCourses();
+  console.log('Seeding professors...');
+  await seedProfessors();
 }
 
 main()
